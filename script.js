21bb95a7a1d02dec9cfffccebd5c974fb9ad55bb
mimiezcurra <141961862+mimiezcurra@users.noreply.github.com>
2023-10-09 21:11:43 -0300

    a

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+function IMC (peso,altura){return peso/altura}
+
+function comparaciones (valor1, valor2) 
+{return set (comparaciondebil = valor1 == valor2) , set (comparacionestricta = valor1 === valor2)}
+
+function par (valor) {return (valor%2)== 0 }
+
+prueba1 IMC (176, 26)g
