9e7323a42ed2e4ae61d4e1c801e9d52e7c502664
mimiezcurra <141961862+mimiezcurra@users.noreply.github.com>
2023-11-13 11:54:51 -0300

    13/11

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 
+/*
+login 
+<project title="Conexion">
+        <service name="MongoDB Atlas" ty
+*/
 
-function IMC (peso,altura){return peso/altura}
 
-function comparaciones (valor1, valor2) 
-{return set (comparaciondebil = valor1 == valor2) , set (comparacionestricta = valor1 === valor2)}
 
-function par (valor) {return (valor%2)== 0 }
-
-prueba1 IMC (176, 26)g
+Swal.fire({
+        title: "Do you want to save the changes?",
+        showCancelButton: true,
+        confirmButtonText: "Save",
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          Swal.fire("Saved!", "", "success");
+        } else if (result.isDenied) {
+          Swal.fire("Changes are not saved", "", "info");
+        }
+      });
