faf18d68f3c0e74075ec7a10d6109d931893b047
mimiezcurra <141961862+mimiezcurra@users.noreply.github.com>
2023-11-24 23:48:44 -0300

    24/11 23.48

--- script.js
+++ script.js
@@ -1,43 +1,20 @@
-
-/*
-login 
-<project title="Conexion">
-        <service name="MongoDB Atlas" ty
-*/
-
-
-function mostrarMensaje() {
 Swal.fire({
-        title: "Do you want to save the changes?",
-        showCancelButton: true,
-        confirmButtonText: "Save",
-      }).then((result) => {
-        /* Read more about isConfirmed, isDenied below */
-        if (result.isConfirmed) {
-          Swal.fire("Saved!", "", "success");
-        } else if (result.isDenied) {
-          Swal.fire("Changes are not saved", "", "info");
-        }
-      })};
-
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('form[role="search"]').addEventListener('submit', function(event) {
-        event.preventDefault(); // Evita el comportamiento predeterminado del formulario
-
-        const searchText = document.querySelector('input[type="search"]').value.toLowerCase();
-        const recipes = document.querySelectorAll('.cuadro-receta');
-
-        recipes.forEach(function(recipe) {
-            const recipeName = recipe.querySelector('h1').textContent.toLowerCase();
-            const recipeId = recipe.getAttribute('id');
-
-            if (recipeName.includes(searchText)) {
-                window.location.href = `#${recipeId}`;
-            }
-        });
-    });
+  title: 'Error!',
+  text: 'Do you want to continue',
+  icon: 'error',
+  confirmButtonText: 'Cool'
 });
 
+const { value: text } = await Swal.fire({
+  input: "textarea",
+  inputLabel: "Message",
+  inputPlaceholder: "Type your message here...",
+  inputAttributes: {
+      "aria-label": "Type your message here"
+  },
+  showCancelButton: true
+});
+
+if (text) {
+  Swal.fire(text);
+}
